468cf591b8ed3c873f7e23f579ad060c9707d917
Alexander Ortiz <undergrounder16@gmail.com>
2024-03-10 01:20:07 -0500

    initial textheme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static String appName = "MovieApp";
-  static const Color _primaryColor = Color.fromRGBO(50, 168, 115, 1);
-  static const Color _bodyColor = Color.fromRGBO(12, 12, 12, 1);
+  static const Color primaryColor = Color.fromRGBO(50, 168, 115, 1);
+  static const Color bodyColor = Color.fromRGBO(12, 12, 12, 1);
+  static const Color titleColor = Colors.white;
 
   ThemeData getTheme() => ThemeData(
       useMaterial3: true,
-      primaryColor: _primaryColor,
+      primaryColor: primaryColor,
       fontFamily: 'Lato',
+      textTheme: const TextTheme(
+        titleMedium: TextStyle(
+          color: Colors.white,
+          fontSize: 24
+        )
+      ),
       textButtonTheme: TextButtonThemeData(
         style: TextButton.styleFrom(
-          backgroundColor: _primaryColor,
-          foregroundColor: _bodyColor,
+          backgroundColor: primaryColor,
+          foregroundColor: bodyColor,
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(5))
           )
@@ -20,9 +27,9 @@ class AppTheme {
       ),
       outlinedButtonTheme: OutlinedButtonThemeData(
         style: OutlinedButton.styleFrom(
-          foregroundColor: _primaryColor,
-          surfaceTintColor: _primaryColor,
-          side: const BorderSide(color: _primaryColor),
+          foregroundColor: primaryColor,
+          surfaceTintColor: primaryColor,
+          side: const BorderSide(color: primaryColor),
           shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5)))
         )
       )
