598ebc3c76f16c870128248569873592fe517558
Alexander Ortiz <undergrounder16@gmail.com>
2024-03-10 01:20:21 -0500

    add login router

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:movie/presentation/screens/login_screen/login_screen.dart';
 import 'package:movie/presentation/screens/welcome_screen/welcome_screen.dart';
 
 final appRouter = GoRouter(
@@ -8,6 +9,11 @@ final appRouter = GoRouter(
           path: '/',
           name: WelcomeScreen.name,
           builder: (context, state) => const WelcomeScreen()
+      ),
+      GoRoute(
+          path: '/login',
+          name: LoginScreen.name,
+          builder: (context, state) => const LoginScreen()
       )
     ]
 );
